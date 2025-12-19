58bbe02a33d1d3b2df68182fe4e20628c8eb9092
Suman Jyoti Phukan <mystogan3780@gmail.com>
2025-12-19 16:27:41 +0530

    Chore Cors : Reconfigured cors

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    credentials: false,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 );
 
